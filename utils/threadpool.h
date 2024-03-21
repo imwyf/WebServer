3a9da24caf005226fa8b9e03191a99259a00bd52
imwyf <imwyf@I-302.localdomain>
2024-03-21 11:20:09 +0800

    线程池改进

--- utils/threadpool.h
+++ utils/threadpool.h
@@ -10,66 +10,81 @@
 
 /**
  * 半同步/半反应堆线程池:内包含一个工作队列，主线程（线程池的创建者）往工作队列中插入任务，工作线程通过竞争来取得任务并执行它。
- * T：封装好的http_connector类，包含了该http连接的所有信息
  */
-template <typename T>
 class ThreadPool {
-public: // 对外暴露的接口
+public:
     /**
      * thread_count：线程池中线程的数量
      */
     explicit ThreadPool(size_t thread_count = 8)
-        : m_pool(std::make_shared<Pool>())
+        : m_stop(false)
     {
         assert(thread_count > 0);
         for (size_t i = 0; i < thread_count; i++) { // 创建thread_count个线程
-            std::thread([pool = m_pool] { // 使用lambda表达式定义线程执行的run函数
-                std::unique_lock<std::mutex> locker(pool->mtx); // 加锁，防止多个线程取到同一个任务
-                while (true) { // 死循环
-                    if (!pool->task_queue.empty()) { // 若工作队列有任务
-                        auto task = std::move(pool->task_queue.front());
-                        pool->task_queue.pop();
-                        locker.unlock(); // 对任务队列操作结束释放锁
-                        task(); // 执行函数
-                        locker.lock(); // 函数执行完毕，重新加锁
-                    } else if (pool->isClosed)
-                        break;
-                    else
-                        pool->cond.wait(locker); // 线程阻塞等待被唤醒
-                }
-            })
-                .detach();
+            m_workers.emplace_back( // 线程加入workers数组
+                std::thread( // 新建的线程对象
+                    [this] { // 使用lambda表达式定义线程执行的run函数
+                        // std::unique_lock<std::mutex> locker(this->m_queue_mutex);
+                        while (true) { // 死循环
+                            std::function<void()> task;
+
+                            {
+                                std::unique_lock<std::mutex> locker(this->m_queue_mutex); // 加锁，RAII风格的锁可以保证当该lambda表达式退出时，解锁
+                                this->m_condition.wait(locker,
+                                    [this] { return this->m_stop || !this->m_tasks.empty(); }); // 用lambda表达式定义判断任务队列是否有任务的谓词
+                                if (this->m_stop && this->m_tasks.empty())
+                                    return;
+                                task = std::move(this->m_tasks.front()); // move取出任务更高效
+                                this->m_tasks.pop();
+                            }
+
+                            task(); // 执行任务
+                        }
+
+                        // 另一版本的run函数
+                        // if (!this->m_tasks.empty()) { // 若工作队列有任务
+                        //     auto task = std::move(this->m_tasks.front());
+                        //     this->m_tasks.pop();
+                        //     locker.unlock(); // 对任务队列操作结束释放锁
+                        //     task(); // 执行函数
+                        //     locker.lock(); // 函数执行完毕，重新加锁
+                        // } else if (this->m_stop)
+                        //     break;
+                        // else
+                        //     this->m_condition.wait(locker); // 线程阻塞等待解锁被唤醒
+                    }));
+            m_workers.back().detach(); // 线程分离式启动，主线程不用等待子线程结束
         }
     }
     ~ThreadPool()
     {
-        if (static_cast<bool>(m_pool)) {
-            {
-                std::lock_guard<std::mutex> locker(m_pool->mtx);
-                m_pool->isClosed = true;
-            }
-            m_pool->cond.notify_all();
+        {
+            std::lock_guard<std::mutex> locker(m_queue_mutex);
+            m_stop = true;
         }
+        m_condition.notify_all();
     };
 
     /**
-     * 往工作队列中添加任务：先获取锁，添加任务，唤醒一个线程
+     * 往工作队列中添加任务，唤醒一个线程，task应是一个右值，匿名函数，lambda表达式
      */
+    template <typename T>
     void AddTask(T&& task)
     {
-        std::lock_guard<std::mutex> locker(m_pool->mtx); // 加入队列时加锁
-        m_pool->task_queue.emplace(std::forward<T>(task)); // 将任务添加到队列中
-        m_pool->cond.notify_one(); // 唤醒一个线程
+        {
+            std::lock_guard<std::mutex> locker(m_queue_mutex); // 加入队列时加锁
+            m_tasks.emplace(std::forward<T>(task)); // 将任务添加到队列中
+        }
+        m_condition.notify_one(); // 唤醒一个线程
     }
 
-private: // 私有类属性
-    struct Pool { // 线程池定义，内部包含锁
-        std::mutex mtx; // 锁
-        std::condition_variable cond; // 条件变量
-        bool isClosed = false; // 判断是否关闭线程
-        std::queue<std::function<void()>> task_queue; // 任务队列
-    };
-    std::shared_ptr<Pool> m_pool; // 指向线程池的指针，多个线程共享该内存，因此shared_ptr
+private:
+    std::vector<std::thread> m_workers; // 线程数组
+    std::queue<std::function<void()>> m_tasks; // 任务队列
+
+    std::mutex m_queue_mutex; // 互斥量
+    std::condition_variable m_condition; // 条件变量
+    bool m_stop; // 停止或开始任务
 };
 
 #endif
